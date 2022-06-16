12a9399fad66c0ab85a7fcfc4738d37baf53ac3f
Manav <manavmalviya0309@gmail.com>
2022-06-16 16:04:45 +0530

    added sql file to create first database and table

---
+++ initialize.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE groupchat_testing
+
+USE groupchat_testing
+
+CREATE TABLE courses(
+`id` int NOT NULL AUTO_INCREMENT,
+`full` VARCHAR(10),
+`code` VARCHAR(6),
+`title` VARCHAR(150),
+`session` VARCHAR(1),
+`lectures` VARCHAR(200),
+PRIMARY KEY(`id`),
+INDEX (`code`)
+);
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+
+flush privileges;
