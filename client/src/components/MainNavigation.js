a6304358cea87f3dc2c0aa12d86337ff6b093892
Manav <manavmalviya0309@gmail.com>
2022-06-19 23:52:56 +0530

    added logo

--- client/src/components/MainNavigation.js
+++ client/src/components/MainNavigation.js
@@ -1,28 +1,40 @@
 import {
   AppBar,
   Toolbar,
-  Typography,
   ThemeProvider,
   Stack,
   Button,
+  Box,
 } from "@mui/material";
 import theme from "./Theme";
+import fullLogo from "./uoftgroupchatslogofull.PNG";
 
 const MainNavigation = () => {
   return (
     <ThemeProvider theme={theme}>
       <AppBar position="fixed" color="primary">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            UofT GroupChats
-          </Typography>
-          <Stack direction="row" spacing={2}>
-            <Button color="inherit">DONATE</Button>
-            <Button color="inherit">GITHUB</Button>
-          </Stack>
+          <Box sx={{ flexGrow: 1 }}>
+            <a href="/">
+              <Box
+                component="img"
+                sx={{
+                  height: 48,
+                }}
+                alt="Your logo."
+                src={fullLogo}
+              />
+            </a>
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Stack direction="row" spacing={2}>
+              <Button color="inherit">DONATE</Button>
+              <Button color="inherit">GITHUB</Button>
+            </Stack>
+          </Box>
         </Toolbar>
       </AppBar>
-    </ThemeProvider>
+    </ThemeProvider >
   );
 };
 
