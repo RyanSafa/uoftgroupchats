30cd365f20abefcc7baf532655a448d7e7052754
Manav <manavmalviya0309@gmail.com>
2022-06-25 19:19:02 +0400

    added sections component

---
+++ client/src/components/Sections.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Tab from 'react-bootstrap/Tab'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Sections = (props) => {
+    const sections_list = ['Common', ...props.lectures]
+
+    const list_output = sections_list.map((lec) => {
+        return <ListGroup.Item variant='secondary-blue' action href={`#${lec}`}>
+            {lec}
+        </ListGroup.Item>
+    })
+    const links_output = sections_list.map((lec) => {
+        return <Tab.Pane eventKey={`#${lec}`}>
+            {lec}
+        </Tab.Pane>
+    })
+
+    return (
+        <Container className='mt-2'>
+            <Card className="text-center" bg='primary-blue' text='white'>
+                <Card.Body>
+                    <Card.Title>Find your groupchat!</Card.Title>
+                    <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+                        <Row>
+                            <Col sm={4}>
+                                <ListGroup>
+                                    {list_output}
+                                </ListGroup>
+                            </Col>
+                            <Col sm={8}>
+                                <Tab.Content>
+                                    {links_output}
+                                </Tab.Content>
+                            </Col>
+                        </Row>
+                    </Tab.Container>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+}
+
+export default Sections
--- client/src/pages/CourseDetail.js
+++ client/src/pages/CourseDetail.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
-
+import Sections from '../components/Sections'
 
 const CourseDetail = (props) => {
     const params = useParams()
@@ -25,10 +23,13 @@ const CourseDetail = (props) => {
     }
 
     return (
-        <Card className="p-2 align-items-center rounded" bg="secondary-blue" text="white">
-            <h1 className="display-1">{course.code}</h1>
-            <h2 className="display-6">{course.title}</h2>
-        </Card>
+        <>
+            <Card className="p-2 align-items-center rounded" bg="secondary-blue" text="white">
+                <h1 className="display-1 fw-bold">{course.code}</h1>
+                <h2 className="display-6">{course.title}</h2>
+            </Card>
+            <Sections lectures={course.lectures} />
+        </>
     )
 }
 
