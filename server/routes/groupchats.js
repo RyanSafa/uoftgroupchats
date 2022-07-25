044efd2ffa7482fe83be525ed11a3537350a2975
RyanSafa <ryansafa2@gmail.com>
2022-07-25 03:21:05 -0500

    add rate limiting

--- server/routes/groupchats.js
+++ server/routes/groupchats.js
@@ -6,11 +6,24 @@ const {
 } = require("../validationSchemas/groupchatValidationSchema");
 const { validateRequest } = require("../middleware/requestValidation");
 const { checkSchema } = require("express-validator");
-
+const rateLimit = require("express-rate-limit");
+const createGroupChatLimiter = rateLimit({
+  windowMs: 10800000, // 3 hours
+  max: 6, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message:
+    "To many group chats created by one user. Please try again after three hours",
+  skipFailedRequests: true,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (req, res, next, options) => {
+    next({ status: options.statusCode, message: options.message });
+  },
+});
 router.post(
   "/",
   checkSchema(groupchatSchema),
   validateRequest,
+  createGroupChatLimiter,
   groupchatsController.createGroupchat
 );
 
--- server/routes/reports.js
+++ server/routes/reports.js
@@ -6,11 +6,24 @@ const {
 } = require("../validationSchemas/reportValidationSchema");
 const { validateRequest } = require("../middleware/requestValidation");
 const { checkSchema } = require("express-validator");
-
+const rateLimit = require("express-rate-limit");
+const createReportLimiter = rateLimit({
+  windowMs: 86400000, // 24 hours
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message:
+    "Too many reports created by one user. Please try again after 24 hours",
+  skipFailedRequests: true,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (req, res, next, options) => {
+    next({ status: options.statusCode, message: options.message });
+  },
+});
 router.post(
   "/",
   checkSchema(reportSchema),
   validateRequest,
+  createReportLimiter,
   reportsController.createReport
 );
 
